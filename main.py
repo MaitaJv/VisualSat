3d807b2a2af25716bd90998b1c085dad963bb5da
MaitaJv <javiermaita@hotmail.com.ar>
2025-10-03 11:29:13 -0300

    actulizacion 3 oct - desarrollo de front y visualizacion de imagenes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -5,14 +7,17 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+images_raw_path = "/home/user/Documentos/Facultad/PID/Processing-IMG/processed-images"
+
 # Montar la carpeta "static" para servir archivos como imágenes, CSS, JS
 app.mount("/static", StaticFiles(directory="static"), name="static")
 # Montamos la carpetas images-raw que contiene las imagenes tomadas por los sats
-app.mount("/images-raw", StaticFiles(directory="/home/user/Documentos/Facultad/PID/Processing-IMG/processed-images"), name="images-raw")
+app.mount("/images-raw", StaticFiles(directory=images_raw_path), name="images-raw")
 
 # Motor de plantillas
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    images = os.listdir(images_raw_path)
+    return templates.TemplateResponse("home.html", {"request": request, "images": images})
