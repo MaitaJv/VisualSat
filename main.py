b3c0161ef88c0b6fb60d4687ea44b8bbdc53e514
MaitaJv <javiermaita@hotmail.com.ar>
2025-10-03 10:41:50 -0300

    primer commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+# Montar la carpeta "static" para servir archivos como imágenes, CSS, JS
+app.mount("/static", StaticFiles(directory="static"), name="static")
+# Montamos la carpetas images-raw que contiene las imagenes tomadas por los sats
+app.mount("/images-raw", StaticFiles(directory="/home/user/Documentos/Facultad/PID/Processing-IMG/processed-images"), name="images-raw")
+
+# Motor de plantillas
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
